1f7de95d5194c8cce584ca81b0903e51ebff19a4
Oktado Putra <oktado.putra@gmail.com>
2021-02-10 17:23:01 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import moment from "moment";
+import axios from "axios";
+
+const instance = {
+  key: "5aeb7a4c5cd892d28307bcb471d2f344",
+  base_url: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const API = `${instance.base_url}weather?q=${query}&units=metric&APPID=${instance.key}`;
+
+  const search = async (e) => {
+    try {
+      if (e.key === "Enter") {
+        const response = await axios.get(API);
+        setWeather(response.data);
+        setQuery("");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const date = moment().format("MMMM Do YYYY");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 16
+            ? "app warm"
+            : "app"
+          : "app"
+      }
+    >
+      <main>
+        <div className="search-box">
+          <input
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+            type="text"
+            className="search-bar"
+            placeholder="search city or country ..."
+          />
+        </div>
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{date}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°c</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </main>
     </div>
   );
 }
---
+++ src/constant.js
@@ -0,0 +1,4 @@
+const API = {
+  key: "524901&appid=5aeb7a4c5cd892d28307bcb471d2f344",
+  base_url: "https://api.openweathermap.org/data/2.5/",
+};
